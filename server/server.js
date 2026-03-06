9a33aa8401ae774cbada19f9e1b0559af785d076
Shravan Agrawal <shravanagrawal04443@gmail.com>
2026-03-06 16:40:06 +0530

    update server port and socket url

--- server/server.js
+++ server/server.js
@@ -106,6 +106,8 @@ io.to(room).emit("room_users",rooms[room])
 
 })
 
-server.listen(5000,()=>{
-console.log("Server running on port 5000")
+const PORT = process.env.PORT || 5000
+
+server.listen(PORT,()=>{
+console.log("Server running on port " + PORT)
 })
